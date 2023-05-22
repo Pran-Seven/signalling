a9897d29a2fb778ce519aefa6a7507422f76eae3
ad4545 <adarsh.kumar@sevenrobotics.in>
2023-05-22 20:31:57 +0530

    allowed all origins

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 app.use(express.json())
 app.use(cors({
-    origin:'https://sevebhub-stable.web.app'
+    origin:"*"
 }))
 
 app.get('/',(req,res)=>{
@@ -17,7 +17,7 @@ app.get('/',(req,res)=>{
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 const server = app.listen(PORT,()=>[
     console.log('connected')
@@ -26,7 +26,7 @@ const server = app.listen(PORT,()=>[
 
 const io = require('socket.io')(server,{
     cors:{
-        origin:'https://sevebhub-stable.web.app'
+        origin:"*"
     }
 })
 
