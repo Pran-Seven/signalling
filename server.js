5eab9575cc9d3538d2d6b9a98a519819316341a4
ad4545 <adarsh.kumar@sevenrobotics.in>
2023-06-02 14:33:17 +0530

    bug-fix-5

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ io.on('connection',(socket)=>{
          const id = newMessage.userId
          console.log(finalObj,'final-Obj')
          socket.in(id).emit('received',finalObj)
-         finalObj={}
+        //  finalObj={}
      })
 })
